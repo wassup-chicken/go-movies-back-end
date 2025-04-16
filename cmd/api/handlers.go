c8893de4962002ec13635279a137517222761fc8
Wassup Chicken <74932143+wassup-chicken@users.noreply.github.com>
2025-04-15 21:31:28 -0400

    first commit

---
+++ cmd/api/handlers.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (app *application) Home(w http.ResponseWriter, r *http.Request) {
+	var payload = struct {
+		Status  string `json:"status"`
+		Message string `json:"message"`
+		Version string `json:"version"`
+	}{
+		Status:  "active",
+		Message: "Go Movies up and running",
+		Version: "1.0.0",
+	}
+
+	_ = app.writeJSON(w, http.StatusOK, payload)
+}
+
+func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
+	movies, err := app.DB.AllMovies()
+
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	_ = app.writeJSON(w, http.StatusOK, movies)
+}
