3308755f363d4a92f97aacc07a9842d33cd047d8
Wassup Chicken <74932143+wassup-chicken@users.noreply.github.com>
2025-05-05 22:27:48 -0400

    update cors

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -4,17 +4,23 @@ import "net/http"
 
 func (app *application) enableCORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "https://mustx.co")
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		origin := r.Header.Get("Origin")
+		if origin == "https://mustx.co" {
+			// Echo back the incoming origin
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Vary", "Origin")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+		}
 
-		if r.Method == "OPTIONS" {
-			// w.Header().Set("Access-Control-Allow-Credentials", "true")
+		if r.Method == http.MethodOptions {
+			// Preflight request
 			w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
 			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, X-CSRF-Token, Authorization")
+			w.WriteHeader(http.StatusNoContent)
 			return
-		} else {
-			h.ServeHTTP(w, r)
 		}
+
+		h.ServeHTTP(w, r)
 	})
 }
 
